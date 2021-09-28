2d834bfd970f62cfae447e3c7078be7f940d0409
killcrafter1 <kilian-hauber@gmx.de>
2021-09-28 22:11:07 +0200

    2 side layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        darkblue: '#15192A',
+        mainblue: '#1F263C',
+        lightgray: '#737B92',
+        grayblue: '#1A1F32',
+        herugreen: '#00D084',
+        herured: '#ED4245',
+        discordblue: '#5865F2',
+        ultrablue: '#0076FF'
+      },
+      fontFamily: {
+        nunito: ["'Nunito', sans-serif"],
+        delagothicone: ["'Dela Gothic One', cursive"]
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
 }
